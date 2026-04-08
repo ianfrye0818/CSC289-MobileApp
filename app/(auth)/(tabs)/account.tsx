c0aea99dcd92c714dd69e3f9095b9eab138566c1
Timothy Carpenter <timothycarpenternc@gmail.com>
2026-04-08 17:42:49 -0400

    Test Account Screen

--- app/(auth)/(tabs)/account.tsx
+++ app/(auth)/(tabs)/account.tsx
@@ -1,10 +1,5 @@
-import { Text } from '@/components/ui/text';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import AccountScreen from "@/features/account/components/AccountScreen";
 
 export default function AuthAccountScreen() {
-  return (
-    <SafeAreaView className='flex-1 bg-background p-4'>
-      <Text>Account</Text>
-    </SafeAreaView>
-  );
+  return <AccountScreen />;
 }
