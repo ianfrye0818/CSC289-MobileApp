4b56bd5ccfdf77ce7d1647b02bbdcf16a9e2bb5f
Ian Frye <ianfrye.dev@gmail.com>
2026-04-26 11:07:40 -0400

    fixed server build errors, added dockerfile and .dockerignore file. added server build script

--- server/src/features/customers/Customer.controller.ts
+++ server/src/features/customers/Customer.controller.ts
@@ -1,18 +1,27 @@
 import { User } from '@/decorators/User.decorator';
 import { UpdatedMessageResponse } from '@/types/MessageReponse.type';
-import { Body, Controller, Get, Param, Patch, } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch } from '@nestjs/common';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
-import { ApiBody, ApiOkResponse, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBody,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
 import { AuthUserDto } from '../auth/types/AuthUserDto.type';
 import { UpdateCustomerCommand } from './commands/UpdateCustomerCommand';
 import { CustomerDetailsResponseDto } from './models/CustomerDetailsResponse.dto';
-import { UpdateCustomerRequestDto } from './models/UpdateCustomerRequest.dto';
 import { GetCurrentCustomerDetailsQuery } from './queries/GetCurrentCustomerDetails/GetCurrentCustomerDetailsQuery';
+import { UpdateCustomerRequestDto } from './UpdateCustomer/UpdateCustomerRequestDto';
 
 @ApiTags('Customers')
 @Controller('customers')
 export class CustomerController {
-  constructor(private readonly queryBus: QueryBus, private readonly commandBus: CommandBus) {}
+  constructor(
+    private readonly queryBus: QueryBus,
+    private readonly commandBus: CommandBus,
+  ) {}
 
   @Get('me')
   @ApiOperation({ summary: 'Get customer details' })
---
+++ server/src/features/customers/UpdateCustomer/UpdateCustomerRequestDto.ts
@@ -0,0 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class UpdateCustomerRequestDto {
+  @ApiProperty({ type: String, required: true })
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @ApiProperty({ type: String, required: false })
+  @IsString()
+  @IsOptional()
+  lastName?: string;
+
+  @ApiProperty({ type: String, required: false })
+  @IsString()
+  @IsOptional()
+  phone?: string;
+
+}
